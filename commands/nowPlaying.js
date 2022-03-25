b5374c916a5cde46b41d646f9b692e547ed2c8e3
Rotekoppen <meg.er.anonym@gmail.com>
2022-03-25 20:17:38 +0100

    Improved nowplaying

--- commands/nowPlaying.js
+++ commands/nowPlaying.js
@@ -8,15 +8,27 @@ module.exports = {
 		.setDescription('Display the current playing track'),
 	async execute(interaction) {
     const player = await this.client.dtune.getPlayer(interaction.guildId)
-		const { queue } = await player.getQueue(0, 1, true)
+		const { queue } = await player.getQueue(0, 1, false)
 
 		if (queue.length < 0) return interaction.reply({ content: emoji("crash") + " There is no music playing.", ephemeral: true })
 		const track = queue[0]
 
+    let ms = Math.floor(track.resource.playbackDuration / 1000)
+    let mmss = ""
+
+    mmss += Math.floor(ms / 60)
+    mmss += ":"
+    let ss = ms % 60
+    if (ss < 10) {
+      ss = "0" + ss
+    }
+    mmss += ss
+
+
 		const embed = new MessageEmbed()
 		.setTitle(`${emoji("dance")} Now playing:`)
 		.setImage(track.thumbnail)
-		.setDescription(`[${track.title} - ${track.lengthFormatted}](${track.url})`)
+		.setDescription(`[${track.metadata.title} - ${mmss} / ${track.metadata.lengthFormatted}](${track.metadata.url})`)
 		//.setFooter("Requested by " + track.requester.username, 'https://cdn.discordapp.com/avatars/' + track.requester.id + "/" + track.requester.avatar + ".webp")
 
 		interaction.reply({ embeds: [embed], ephemeral: true });
